596bdc7dc3f7a0c7e38abb96fe3be3902c305445
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-19 23:23:12 +0530

    [Madhavi] added program for fibonacci series

---
+++ LogicalProgramming/LogicalProgramming/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class FibonacciSeries
+    {
+        public void Fibonacci_Series()
+        {
+            Console.WriteLine("Enter the number of elements to print fabonacci series : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int firstNum = 0, secondNum = 1, nextNum = 0;
+            Console.Write(firstNum + " " + secondNum + " ");
+            for (int i = 2; i < num; i++)
+            {
+                nextNum = firstNum + secondNum;
+                Console.Write(nextNum + " ");
+                firstNum = secondNum;
+                secondNum = nextNum;
+            }
+        }
+    }
+}
---
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LogicalProgramming;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("1.Fibonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number" +
+            "\n5.Coupon Number\n6.StopWatch Program\n7.Vending Maching\n8.Day Of Week\n9.Temperature"+ 
+            "Conversion\n10.Monthly Payment.\n11Squart Root.\n12.Convert To Binary\n13.Swap Nibbles");
+
+        Console.WriteLine("Enter option ");
+        int option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                FibonacciSeries f = new FibonacciSeries();
+                f.Fibonacci_Series();
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+            case 12:
+
+                break;
+            case 13:
+
+                break;
+            default:
+                Console.WriteLine("You entered wrong option");
+                break;
+        }
+    }
+}
