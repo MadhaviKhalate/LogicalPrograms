e7ac10bc3e5d17002e8f21ef97d328ef796c97f2
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-20 13:47:37 +0530

    [Madhavi] added program for CouponCode

--- LogicalProgramming/LogicalProgramming/CouponNumber.cs
+++ LogicalProgramming/LogicalProgramming/CouponNumber.cs
@@ -12,23 +12,20 @@ namespace LogicalProgramming
         public void DistinctCoupon()
         {
             Random rand = new Random();
-            int max = 100000, number, randNum, i;
+			int max = 100, number, i, randNum;
 			String couponCode;
-			char[] chars="andhftruehebfwad".ToCharArray();
+			char[] chars = "3284hjfd7234dkfjghdkfkdjf2234cdc".ToCharArray();
 			Console.WriteLine("Enter value for number of distinct coupon you want ");
 			number = Convert.ToInt32(Console.ReadLine());
 			for (i = 0; i < number; i++)
 			{
 				StringBuilder sb = new StringBuilder();
 				randNum = Convert.ToInt32(rand.Next() * max);
-			
 				while (randNum > 0)
 				{
 					sb.Append(chars[randNum % chars.Length]);
 					randNum /= chars.Length;
-					//Console.WriteLine(" Distinct Coupon generated : " + i + "number of times");
 				}
-
 				couponCode = sb.ToString();
 				Console.WriteLine("Coupon Code: " + couponCode);
 			}
