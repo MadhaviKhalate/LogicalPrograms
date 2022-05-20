4b125ee052aa96f3bfce35a65861615b263fe871
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-20 18:35:21 +0530

    [Madhavi] made changes in coupons number

--- LogicalProgramming/LogicalProgramming/CouponNumber.cs
+++ LogicalProgramming/LogicalProgramming/CouponNumber.cs
@@ -8,8 +8,10 @@ namespace LogicalProgramming
 {
 	internal class CouponNumber
 	{
-         
-        public void DistinctCoupon()
+        int CouponCount = 0, UniqCouponCnt = 0, number;
+        Random rand = new Random();
+        List<int> couponNumber = new List<int>();
+        /*public void DistinctCoupon()
         {
             Random rand = new Random();
 			int max = 100, number, i, randNum;
@@ -29,6 +31,29 @@ namespace LogicalProgramming
 				couponCode = sb.ToString();
 				Console.WriteLine("Coupon Code: " + couponCode);
 			}
-		}
-	}
+            int CouponCount = 0, UniqueCouponCount = 0, i = 0;
+            Random Random = new Random();
+            List<int> couponCodes = new List<int>();      
+        }*/
+        public void DistinctCoupon()
+        {
+            Console.WriteLine("Enter value for number of distinct coupon you want ");
+            number = Convert.ToInt32(Console.ReadLine());
+            while (UniqCouponCnt < number)
+            {
+                int couponNo = rand.Next();
+                CouponCount++;
+                if (!couponNumber.Contains(couponNo))
+                {
+                    couponNumber.Add(couponNo);
+                    UniqCouponCnt++;
+                }
+            }
+            foreach (int couponNum in couponNumber)
+            {
+                Console.WriteLine(couponNum);
+            }
+            Console.WriteLine("Number of times coupon generated is " + CouponCount);
+        }
+    }
 }
